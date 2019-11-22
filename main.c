697d13ce4f2cfccedbf5aa21f6dc2d11d6cdf3a3
EMERICKCHALET <emerickchalet@gmail.com>
2019-11-22 14:52:58 +0100

    Exercices première partie

---
+++ main.c
@@ -0,0 +1,75 @@
+//
+//  main.c
+//  monprojet
+//
+//  Created by Emerick CHALET on 15/09/2019.
+//  Copyright © 2019 Emerick CHALET. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+double aireRectangle(double largeur, double hauteur);
+
+
+int main(int argc, char *argv[])
+{
+  /*  MCDO exercice
+   int reponse = 0;
+  printf("=== Menu ===\n");
+  printf("1. Royal Cheese\n");
+  printf("2. Mc Deluxe\n");
+  printf("3. Mc Bacon\n");
+  printf("4. Big Mac\n");
+  printf("Votre choix ?\n");
+  scanf("%d",&reponse);
+
+  switch (reponse)
+  {
+  case 1:
+    printf("Royal Cheese\n");
+    break;
+  
+case 2:
+    printf("Mc Deluxe\n");
+    break;
+
+case 3:
+    printf("Mc Bacon\n");
+    break;
+
+ case 4:
+    printf("Big Mac\n");
+    break;   
+
+  default:
+    printf("t'as pas bien choisis\n");
+    break;
+  }*/
+
+     /*Devine Exercice
+     const int MAX = 100, MIN = 1;
+    
+    srand((unsigned int)time(NULL));
+    int nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
+    int chance = 0;
+    
+    while (chance != nombreMystere) {
+         printf("Quel est le nombre ? ");
+         scanf("%d",&chance);
+        if (chance < nombreMystere) {
+            printf("c'est plus \n");
+        }
+        else if (chance > nombreMystere){
+            printf("c'est moins \n");
+        }else{
+            printf("cest bon t'as trouvé... \nte la pete pas non plus \n");
+        }
+    }*/
+    
+  return 0;
+}
+
+
